e3591f2ab2a86d383bddff0e78b8433b9c008d73
Chourasia, Ashok <ashok.chourasia@vce.com>
2017-01-01 12:39:45 +0530

    added grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,86 @@
  */
 module.exports = function (grunt) {
     // Project configuration.
+    var srcPath = 'public_html/app/';
+    var distJsPath = 'dist/angular-phonecat/app.js';
+    var distPath = 'dist/angular-phonecat/';
+    console.log(srcPath);
     grunt.initConfig({
+        srcPath: srcPath,
+        distJsPath: distJsPath,
+        distPath: distPath,
+        concat: {
+            options: {
+                sourceMap: false,
+                separator: '\n',
+                stripBanners: true
+            },
+            js: {
+                src: ['<%=srcPath%>/*.module.js', '<%=srcPath%>/*.config.js',
+                    '<%= srcPath %>/**/*.module.js', '<%= srcPath %>/**/*.js'
+                ],
+                dest: '<%=distJsPath %>'
+            }
+        },
+        copy: {
+            main: {
+                files: [{
+                        expand: true,
+                        cwd: '<%= srcPath %>/',
+                        src: ['*.html', '*.css'],
+                        dest: '<%= distPath %>/'
+                    }, {
+                        expand: true,
+                        cwd: '<%= srcPath %>/',
+                        src: ['*/**/*.html'],
+                        dest: '<%= distPath %>/',
+                        filter: 'isFile'
+                    }, {
+                        expand: true,
+                        cwd: '<%= srcPath %>/',
+                        src: ['phones/*', 'img/**/*'],
+                        dest: '<%= distPath %>/',
+                        filter: 'isFile'
+                    },
+                    {
+                        expand: true,
+                        cwd: '<%= srcPath%>/../',
+                        src: ['lib/*','bootstrap/**/*'],
+                        dest: '<%= distPath %>/',
+                        filter: 'isFile'
+                    }
+                ]
+            }
+        },
+        processhtml: {
+            options: {
+                process: true
+            },
+            dist: {
+                files: {
+                    '<%= distPath %>/index.html': ['<%= distPath %>/index.html']
+                }
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 9001,
+                    base: 'dist/angular-phonecat/',
+                     keepalive: true
+                }
+            }
+        },
+        clean: {
+            build: ['dist/*']
+        }
+
     });
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-processhtml');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.registerTask('default', ['clean','concat','copy', 'processhtml','connect']);
+     grunt.registerTask('build', ['clean','concat','copy', 'processhtml']);
 };
