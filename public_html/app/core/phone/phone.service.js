7a2b5fe041cd398386aca25f2361c082d71664f5
Chourasia, Ashok <ashok.chourasia@vce.com>
2017-01-17 07:44:56 +0530

    added test file

--- public_html/app/core/phone/phone.service.js
+++ public_html/app/core/phone/phone.service.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -12,14 +12,14 @@ angular.
            getPhoneList:function(){
                return $http({
                 method: 'Get',
-                url: '/phones/phones.json'
+                url: 'phones/phones.json'
             });
            },
            getPhoneDetail:function(phoneId){
               return $http({
                 method: 'Get',
-                url: '/phones/'+phoneId+'.json'
-            }); 
+                url: 'phones/'+phoneId+'.json'
+            });
            }
         };
     }
